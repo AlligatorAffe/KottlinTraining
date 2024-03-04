f6048375cda424d2b3481fd06a1ca78ec4976f29
Alfred Olsson <104556169+AlligatorAffe@users.noreply.github.com>
2024-03-04 15:28:31 +0100

    Im on step 9

--- app/src/main/java/com/loc/newsapp/presentation/common/NewsButton.kt
+++ app/src/main/java/com/loc/newsapp/presentation/common/NewsButton.kt
@@ -23,7 +23,10 @@ fun NewsButton(
     ),
         shape = RoundedCornerShape(size = 6.dp)
     ) {
-        Text(text = text, style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold))
+        Text(text = text, style =
+        MaterialTheme.typography
+            .labelMedium
+            .copy(fontWeight = FontWeight.SemiBold))
         
     }
 }
---
+++ app/src/main/java/com/loc/newsapp/presentation/nvgraph/Route.kt
@@ -0,0 +1,3 @@
+package com.loc.newsapp.presentation.nvgraph
+
+sealed class Route
